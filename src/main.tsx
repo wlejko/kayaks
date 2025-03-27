927f13ab4cee6085d29f112fa31cac27cea53561
wlejko <wojciech.lejko@gmail.com>
2025-03-27 12:35:33 +0100

    test message

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
 
-import { routes } from './utils/routes.tsx';
-import { AppLayout } from './shared/components/AppLayout/AppLayout.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          {routes.map((route) => (
-            <Route path={route.path} element={route.element} key={route.id} />
-          ))}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>test</div>
   </StrictMode>
 );
