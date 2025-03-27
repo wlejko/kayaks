dec2477231eb7c60c38fd939ff17788a2a3c2839
wlejko <wojciech.lejko@gmail.com>
2025-03-27 12:33:05 +0100

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  base: '/kayaks/',
+  base: '/kayaks',
   resolve: {
     alias: {
       pages: '/src/pages',
